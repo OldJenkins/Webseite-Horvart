d090d7011b44a7b68557cae0e889cb997d735417
Mace-3105 <65159857+Mace-3105@users.noreply.github.com>
2020-06-03 15:27:47 +0200

    Firebase Package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 /* import { LoginComponent} from '.layout/login/login.component'; */
 import { LoginComponent } from './layout/login/login.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { LoginComponent } from './layout/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    NgxAuthFirebaseUIModule.forRoot(PUT_YOUR_FIREBASE_API_KEY_HERE)
     
   ],
   providers: [],
