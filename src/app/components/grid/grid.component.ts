afc9d55c991ac46d4e0ab1a0eecd80c5de51d6b8
LexAquifolium <lexaquifoliumii@gmail.com>
2020-06-12 12:40:12 +0200

    Add-Funktion für Admin bei Grid
    Add-Funktion für Admin bei Grid

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ImagePost } from '../../models/ImagePost';
-import { ImagepostService } from '../../services/imagepost.service'; 
+import { ImagepostService } from '../../services/imagepost.service';
+import { AdminInformationService } from 'src/app/services/admin-information.service';
 
 
 @Component({
@@ -11,13 +12,22 @@ import { ImagepostService } from '../../services/imagepost.service';
 export class GridComponent implements OnInit {
 
   imagePost: ImagePost[];
+  isAdminLoggedIn: boolean = false;
 
-  constructor(private imagePostService: ImagepostService) {}
+
+  constructor(private imagePostService: ImagepostService, private adminService: AdminInformationService) { }
 
   ngOnInit(): void {
     this.imagePostService.getImagePosts().subscribe(response => {
       this.imagePost = response;
-    })
+    });
+    this.adminService.getIsAdminLoggedIn().subscribe(value => {
+      this.isAdminLoggedIn = value;
+    });
   }
 
+
+  onAddPicToCarouselClicked() {
+
+  }
 }
