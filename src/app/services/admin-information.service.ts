7a3e92dcfec612ca9fcf5a4c04c0c73d562805f5
LexAquifolium <lexaquifoliumii@gmail.com>
2020-06-18 15:39:26 +0200

    WIP
    WIP

--- src/app/services/admin-information.service.ts
+++ src/app/services/admin-information.service.ts
@@ -8,15 +8,26 @@ export class AdminInformationService {
 
   isAdminLoggedIn: BehaviorSubject<boolean>;
 
+  //Benutzer ist eingeloggt - aber kein Admin
+  isUserLoggedIn: BehaviorSubject<boolean>;
+
 
 
   constructor() {
-    this.isAdminLoggedIn = new BehaviorSubject<boolean>(true);
+    this.isAdminLoggedIn = new BehaviorSubject<boolean>(false);
+    this.isUserLoggedIn = new BehaviorSubject<boolean>(false);
+  }
 
+  getIsUserLoggedIn(): Observable<boolean> {
+    return this.isUserLoggedIn;
   }
 
   getIsAdminLoggedIn(): Observable<boolean> {
     return this.isAdminLoggedIn;
+  }
+
+  setIsUserLoggedIn(userIsLoggedIn: boolean) {
+    this.isUserLoggedIn.next(userIsLoggedIn);
   }
 
   setIsAdminLoggedIn(isAdminLoggedIn: boolean) {
