6858db0f09636562b61473bc29ab231ea1242a84
OldJenkins <43578766+OldJenkins@users.noreply.github.com>
2020-06-19 18:00:24 +0200

    implemented crud operations for text stuff

--- src/app/components/secondgrid/text-dialog/text-dialog.component.ts
+++ src/app/components/secondgrid/text-dialog/text-dialog.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TextPost } from '../../../models/TextPost';
 import { TextpostService } from 'src/app/services/textpost.service';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
-import { Observable } from 'rxjs';
-import { finalize, tap } from 'rxjs/operators';
+import { AdminInformationService } from '../../../services/admin-information.service';
 
 
 
@@ -23,21 +20,33 @@ export class TextDialogComponent implements OnInit {
 
   isNew: boolean;
 
+  isAdmin: boolean;
+
   constructor(
     public dialogRef: MatDialogRef<TextDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: TextPost,
     public textpostService: TextpostService,
-    private db: AngularFirestore) {
+    private adminService: AdminInformationService) {
 
-    this.post = data;
+    if (!data.id) {
+      this.isNew = true;
+      console.log("nichts da & es ist neu");
+    } else {
+      this.post = data;
+      console.log("post" + this.post.title);
+    }
+
+
 
     // Check if you are creating a new post
-    if (!this.post.id) {
-      this.isNew = true;
-    }
+
   }
 
   ngOnInit(): void {
+
+    this.adminService.getIsAdminLoggedIn().subscribe(value => {
+      this.isAdmin = value;
+    });
   }
 
   delete() {
